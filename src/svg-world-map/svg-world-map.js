fb04bb7619fb6a9ca40a70d033dd5e199aa4ad48
Chris Vaillancourt <cvaillan22@gmail.com>
2020-06-02 21:25:24 -0700

    finished first draft of svg map

--- src/svg-world-map/svg-world-map.js
+++ src/svg-world-map/svg-world-map.js
@@ -4,6 +4,8 @@ import { geoEqualEarth, geoPath, geoGraticule10 } from 'd3-geo';
 import { select, selectAll } from 'd3-selection';
 import { extent, max } from 'd3-array';
 import { scaleLinear } from 'd3-scale';
+import { format } from 'd3-format';
+import { transition } from 'd3-transition';
 
 console.time('draw map');
 async function createMap() {
@@ -108,6 +110,117 @@ async function createMap() {
         : colorScale(metricValue);
     });
   // step 6) draw peripherals
+  var legendWidth = 120;
+  var legendHeight = 16;
+
+  var legendYTransform =
+    dimensions.width < 800
+      ? dimensions.boundedHeight - 30
+      : dimensions.boundedHeight * 0.5;
+  var legendGroup = wrapper
+    .append('g')
+    .attr('transform', `translate(120, ${legendYTransform})`);
+
+  var legendTitle = legendGroup
+    .append('text')
+    .attr('y', -23)
+    .attr('class', 'legend-title')
+    .text('Population Growth');
+  var legendByline = legendGroup
+    .append('text')
+    .attr('y', -9)
+    .attr('class', 'legend-byline')
+    .text('Percent change in 2017');
+
+  // use defs to store a legend gradient
+  var defs = wrapper.append('defs');
+  // var legendGradientId = 'legend-gradient'
+
+  // svg linearGradient uses a stop-color and offset attribute to interpolate between colors
+  // colorScale.range() creates an array of the colors in our scale
+  // use enter().append() to create one stop per color
+  var legendGradientId = 'legend-gradient';
+  var colorScaleArrayLength = colorScale.range().length;
+  var gradient = defs
+    .append('linearGradient')
+    .attr('id', legendGradientId)
+    .selectAll('stop')
+    .data(colorScale.range())
+    .enter()
+    .append('stop')
+    .attr('stop-color', function getColor(d) {
+      return d;
+    })
+    .attr('offset', function createIndexOffset(d, i) {
+      return `${(i * 100) / colorScaleArrayLength - 1}%`;
+    });
+
+  var legendGradient = legendGroup
+    .append('rect')
+    .attr('x', -legendWidth / 2)
+    .attr('height', legendHeight)
+    .attr('width', legendWidth)
+    .style('fill', `url(#${legendGradientId})`);
+
+  var legendValueRight = legendGroup
+    .append('text')
+    .attr('class', 'legend-value')
+    .attr('x', legendWidth / 2 + 10)
+    .attr('y', legendHeight / 2)
+    .text(`${format('.1f')(maxChange)}%`)
+    .style('text-anchor', 'end');
+
+  var legendValueLeft = legendGroup
+    .append('text')
+    .attr('class', 'legend-value')
+    .attr('x', -legendWidth / 2 - 10)
+    .attr('y', legendHeight / 2)
+    .text(`${format('.1f')(-maxChange)}%`)
+    .style('text-anchor', 'end');
+
+  navigator.geolocation.getCurrentPosition(function getUserLocation(position) {
+    const [x, y] = projection([
+      position.coords.longitude,
+      position.coords.latitude,
+    ]);
+
+    var userLocation = bounds
+      .append('circle')
+      .attr('class', 'my-location')
+      .attr('cx', x)
+      .attr('cy', y)
+      .attr('r', 0)
+      .transition()
+      .duration(500)
+      .attr('r', 10);
+  });
+
+  // step 7) set-up user interaction
+  var tooltip = select('#tooltip');
+  countries
+    .on('mouseenter', handleMouseEnter)
+    .on('mouseleave', handleMouseLeave);
+
+  function handleMouseEnter(datum) {
+    // datum contains the data bound to the hovered element
+    var metricValue = metricDataByCountry.get(countryIdAccessor(datum));
+    tooltip.select('#country').text(countryNameAccessor(datum));
+    tooltip.select('#value').text(`${format(',.2f')(metricValue || 0)}%`);
+
+    // the pathGenerator has a .centroid() method that returns the center of a geoJSON object
+    const [centroidX, centroidY] = pathGenerator.centroid(datum);
+    const x = centroidX + dimensions.margin.left;
+    const y = centroidY + dimensions.margin.top;
+    tooltip.style(
+      'transform',
+      `translate(calc(-50% + ${x}px), calc(-100% + ${y}px))`
+    );
+
+    tooltip.style('opacity', 1);
+  }
+  function handleMouseLeave() {
+    tooltip.style('opacity', 0);
+  }
 }
 
 createMap().then(() => console.timeEnd('draw map'));
