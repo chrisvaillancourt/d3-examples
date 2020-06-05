1c7b94823d6f94e7dd54c1b22ccc02c776f59afe
Chris Vaillancourt <cvaillan22@gmail.com>
2020-06-05 08:19:59 -0700

    imported new.css, base.css, and breadcrumb-nav.css

--- src/interactive-scatter/interactive-scatter.js
+++ src/interactive-scatter/interactive-scatter.js
@@ -1,3 +1,6 @@
+import '../assets/styles/new.css';
+import '../assets/styles/base.css';
+import '../assets/styles/breadcrumb-nav.css';
 import './interactive-scatter.css';
 import * as d3 from 'd3';
 import { Delaunay } from 'd3-delaunay';
